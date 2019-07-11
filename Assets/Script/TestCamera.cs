9ea4901dcb0a5b36d40b22c0824329f12b7ead9c
silverjun <jangej1031@naver.com>
2019-07-11 15:25:28 +0900

    Update Project and make TestCam

---
+++ Assets/Script/TestCamera.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCamera : MonoBehaviour
+{
+    public float _speed = 10.0f;
+    Vector2 _mouseLook;
+    Vector2 _smooth;
+    public float _sensitivity = 5.0f;
+    public float _smoothing = 2.0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void RotateCam()
+    {
+        // rotation
+        var mouseDir = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        mouseDir = Vector2.Scale(mouseDir, new Vector2(_sensitivity * _smoothing, _sensitivity * _smoothing));
+        _smooth.x = Mathf.Lerp(_smooth.x, mouseDir.x, 1.0f / _smoothing);
+        _smooth.y = Mathf.Lerp(_smooth.y, mouseDir.y, 1.0f / _smoothing);
+        _mouseLook += _smooth;
+
+        var quat = Quaternion.AngleAxis(-_mouseLook.y, Vector3.right);
+        transform.rotation = Quaternion.AngleAxis(_mouseLook.x, Vector3.up) * quat;
+    }
+
+    void MoveCam()
+    {
+        // translation
+        float translation = Input.GetAxis("Vertical") * _speed;
+        float straffe = Input.GetAxis("Horizontal") * _speed;
+        translation *= Time.fixedDeltaTime;
+        straffe *= Time.fixedDeltaTime;
+
+        var forward = new Vector3(straffe, 0, translation);
+
+        Vector3.RotateTowards(forward, transform.forward, 0, 0);
+        transform.Translate(forward);
+    }
+
+    void FixedUpdate()
+    {
+        RotateCam();
+        MoveCam();
+
+
+    }
+}
