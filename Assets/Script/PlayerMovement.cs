fdfd4b9bb4c737963c323c727d7171f20bef098d
silverjun <jangej1031@naver.com>
2019-07-12 17:59:48 +0900

    Upload today's work

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Animator _anim;
+    float _speed = 5.0f;
+
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+        _anim.SetBool("IsMoving", true);
+    }
+
+    void FixedUpdate()
+    {
+        _anim.SetFloat("PosX", Input.GetAxis("Horizontal"));
+        _anim.SetFloat("PosY", Input.GetAxis("Vertical"));
+
+        float translation = Input.GetAxis("Vertical") * _speed;
+        float straffe = Input.GetAxis("Horizontal") * _speed;
+        translation *= Time.fixedDeltaTime;
+        straffe *= Time.fixedDeltaTime;
+        
+        transform.Translate(straffe, 0, translation);
+    }
+}
---
+++ Assets/Script/PlayerThrow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerThrow : MonoBehaviour
+{
+    Animator _anim;
+
+    [SerializeField]
+    [Range(100.0f, 750.0f)]
+    float _throwSpeed = 500.0f;     // maybe Range(100, 750);
+
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+        _anim.SetBool("IsMoving", true);
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            StartCoroutine(ThrowKongjyu());
+        }
+    }
+
+    IEnumerator ThrowKongjyu()
+    {
+        _anim.SetLayerWeight(1, 1.0f);
+        yield return new WaitUntil(()=>Input.GetMouseButtonUp(0));
+
+        // throw
+
+        // restore animation layer
+        _anim.SetLayerWeight(1, 0.0f);
+    }
+}
